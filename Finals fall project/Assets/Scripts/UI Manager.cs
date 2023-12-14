a0525443200c62c1ce74d67ef0e2bff54622dfac
RzrRacer88 <146136167+RazrRacer88@users.noreply.github.com>
2023-12-14 14:23:02 -0800

    milestone 3

--- Finals fall project/Assets/Scripts/UI Manager.cs
+++ Finals fall project/Assets/Scripts/UI Manager.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+
+    public TextMeshProUGUI coinAmountText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        coinAmountText.text = "Coin: 0";
     }
 
     // Update is called once per frame
